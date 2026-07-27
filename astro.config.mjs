b178f45cbe5963eb1d435db209d78a4beb14e4de
hogofogo0 <hogofogo0@cheesecakecorp.org>
2026-07-27 18:13:42 +0200

    pls help

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: "https://cheesecakecorp.org",
   base: "/",
-  trailingSlash: "ignore",
-  adapter: cloudflare({ imageService: true }),
+  trailingSlash: "ignore"
 });
